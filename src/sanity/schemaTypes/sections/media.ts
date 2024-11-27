805cff10e13e28f9132e1bb8e9ac4f72a147f7e7
NenadVrtue <hello@vrtuedigital.com>
2024-11-27 12:17:23 +0100

    media section and style config

--- src/sanity/schemaTypes/sections/media.ts
+++ src/sanity/schemaTypes/sections/media.ts
@@ -1,4 +1,5 @@
 import { defineType, defineField } from "sanity";
+import { stringFromPortableText } from "src/utils/stringFromPT";
 
 export default defineType({
   name: "media",
@@ -10,5 +11,35 @@ export default defineType({
       title: "Title",
       type: "pt-string",
     }),
+    defineField({
+      name: "subtitle",
+      title: "Subtitle",
+      type: "pt-string",
+    }),
+    defineField({
+      name: "variation",
+      title: "Variation",
+      type: "string",
+      initialValue: "light",
+      options: {
+        list: [
+          { title: "Light", value: "light" },
+          { title: "Dark", value: "dark" },
+        ],
+      },
+    }),
   ],
+  preview: {
+    select: {
+      title: "title",
+      subtitle: "subtitle",
+    },
+    prepare(selection) {
+      const { title } = selection;
+      return {
+        title: stringFromPortableText(title) || "Media Section Title",
+        subtitle: "Media Section",
+      };
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Dm Sans Variable", ...defaultTheme.fontFamily.sans],
+        display: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        "3xs": ".75rem",
+        "2xs": "0.875rem",
+        xs: "1rem",
+        sm: "1.125rem",
+        md: "1.25rem",
+        lg: "1.375rem",
+        xl: "1.5rem",
+        "2xl": "1.75rem",
+        "3xl": "2rem",
+        "4xl": "2.5rem",
+        "5xl": "3rem",
+        "6xl": "3.25rem",
+        "7xl": "4rem",
+      },
+      colors: {
+        dark: {
+          1: "#050006",
+          2: "#240727",
+        },
+        indigo: "#4E30C5",
+        yellow: "#F4CA51",
+      },
+    },
+  },
+  plugins: [],
+};
