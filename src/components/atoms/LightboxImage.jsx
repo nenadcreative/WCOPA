864f3af2fc2d279e46818c8a706a58b5c1dd9de5
NenadVrtue <hello@vrtuedigital.com>
2025-02-04 21:33:07 +0100

    lightbox

---
+++ src/components/atoms/LightboxImage.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Lightbox } from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css";
+import { urlForImage } from "@sanity/lib/urlForImage";
+
+
+
+export default function LightboxImage({ images, imageUrl }) {
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <>
+            <button
+                type="button"
+                className="flex h-full w-full  "
+                onClick={() => setOpen(true)}
+            >
+                <img src={imageUrl}></img>
+            </button>
+
+            <Lightbox
+                open={open}
+                close={() => setOpen(false)}
+                plugins={[Zoom]}
+                slides={[
+                    { src: imageUrl, width: 8000, height: 6000 },
+                    ...images.map((image) => {
+                        return { src: urlForImage(image).url(), width: 8000, height: 6000 };
+                    }),
+                ]}
+            />
+        </>
+    );
+}
