36b3b767ab976c5a64d87225a52ccb9910e2816d
NenadVrtue <hello@vrtuedigital.com>
2025-02-13 09:36:37 +0100

    Update seo.ts

--- src/sanity/schemaTypes/objects/seo.ts
+++ src/sanity/schemaTypes/objects/seo.ts
@@ -1,19 +1,33 @@
-import { defineType } from "sanity";
+import { defineType, defineField } from "sanity";
 
 export const seoType = defineType({
   name: "seo",
   title: "SEO",
   type: "object",
   fields: [
-    {
+    defineField({
       name: "title",
       title: "Title",
       type: "string",
-    },
-    {
+    }),
+    defineField({
       name: "description",
       title: "Description",
       type: "text",
-    },
+    }),
+    defineField({
+      name: 'noindex',
+      title: 'No Index',
+      description: 'Exclude the page from appearing on search engines',
+      type: 'boolean',
+      initialValue: false
+    }),
+    defineField({
+      name: 'navigation',
+      title: 'Exclude from Navigation',
+      description: 'Exclude the page from appearing on the main navigation',
+      type: 'boolean',
+      initialValue: false
+    })
   ],
 });
