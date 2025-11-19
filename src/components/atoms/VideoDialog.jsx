7192c177caadab819c9e4d7a45fca56ad7abb0a0
NenadVrtue <hello@vrtuedigital.com>
2025-11-19 17:07:20 +0100

    Update VideoDialog.jsx

--- src/components/atoms/VideoDialog.jsx
+++ src/components/atoms/VideoDialog.jsx
@@ -11,15 +11,28 @@ import ReactPlayer from "react-player/lazy";
 
 const VideoDialog = ({ videoUrl, children }) => {
   return (
-    <Dialog className='w-full h-full '>
-      <DialogTrigger className="
-      w-full h-full relative cursor-pointer ">{children}</DialogTrigger>
+    <Dialog className="w-full h-full ">
+      <DialogTrigger
+        className="
+      w-full h-full relative cursor-pointer "
+      >
+        {children}
+      </DialogTrigger>
       <DialogContent className="xl:min-w-[1200px] flex justify-center items-center">
         <DialogTitle className="sr-only">Video</DialogTitle>
         <div className="aspect-video w-full h-full flex justify-center items-center">
-
           {/* @ts-ignore */}
-          {videoUrl ? <ReactPlayer width={1000} height={562.5} controls url={videoUrl} /> : <p>No video available</p>}
+          {videoUrl ? (
+            <ReactPlayer
+              width={1000}
+              height={562.5}
+              controls
+              url={videoUrl}
+              loop
+            />
+          ) : (
+            <p>No video available</p>
+          )}
         </div>
       </DialogContent>
     </Dialog>
